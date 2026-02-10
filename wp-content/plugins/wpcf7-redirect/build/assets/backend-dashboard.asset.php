054fe5c95210e87717c69ee08c3f3d0c575c6499
Tang Edgetech <tang.edgetech@gmail.com>
2026-02-10 09:45:55 +0800

    Delete redirection for cf7

--- wp-content/plugins/wpcf7-redirect/build/assets/backend-dashboard.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3e9b7cd8bef1f9613109');
--- wp-content/plugins/wpcf7-redirect/build/assets/backend-script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '611a50c40c0df41b28c9');
